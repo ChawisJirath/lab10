15580f116490b2fffeccce2ed6f85471bd8aae7b
Wia <chawis_jirath@cmu.ac.th>
2025-12-23 16:10:38 +0700

    lab10 3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,28 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int cnt=0;
+    float sum=0,sumtwo=0;
+    string textline;
+    ifstream source; 
+    source.open("score.txt");
+    while(getline(source,textline)){
+        sum+=atof(textline.c_str());
+        sumtwo+=pow(atof(textline.c_str()),2);
+        cnt++;
+        
+    }
+    float avr=sum/cnt;
+    float sd=sqrt((sumtwo/cnt)-pow(avr,2));
+    source.close();
+    
+    cout << "Number of data = " << cnt << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << avr << endl;
+    cout << "Standard deviation = " << sd << endl;
+}
